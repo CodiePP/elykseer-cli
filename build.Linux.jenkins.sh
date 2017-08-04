18ff0dfff12a2afa16908327f3cf9b6486430c8f
Alexander V. Diemand <codieplusplus@apax.net>
2017-08-04 16:03:59 +0200

    jenkins job

--- build.Linux.jenkins.sh
+++ build.Linux.jenkins.sh
@@ -7,6 +7,6 @@ PLATFRM="Any CPU"
 sn -k LXRrestore/LXRrestore.snk
 sn -k LXRbackup/LXRbackup.snk
 
-msbuild /t:clean /p:Configuration="Debug" /p:Platform="${PLATFRM}" elykseer-cli.Linux.sln
-msbuild /p:Configuration="Debug" /p:Platform="${PLATFRM}" elykseer-cli.Linux.sln
+xbuild /t:clean /p:Configuration="Debug" /p:Platform="${PLATFRM}" elykseer-cli.Linux.sln
+xbuild /p:Configuration="Debug" /p:Platform="${PLATFRM}" elykseer-cli.Linux.sln
 
